2d3279c8fa4ed887f3086b9fe2b25e2f8f165569
NgenItdev2 <dev2.ngenit@gmail.com>
2025-03-16 14:00:02 +0600

    update by ashiquzzaman 15-03-2025

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,21 +1,14 @@
 <?php
-
 namespace App\Providers;
 
-use Exception;
-use App\Models\Area;
-use App\Models\City;
 use App\Models\Admin;
 use App\Models\Brand;
-use App\Models\Store;
-use App\Models\Country;
-use App\Models\Setting;
 use App\Models\Category;
-use App\Models\Division;
-use App\Models\HomePage;
+use App\Models\Setting;
+use Exception;
 use Illuminate\Pagination\Paginator;
-use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -36,49 +29,26 @@ class AppServiceProvider extends ServiceProvider
         View::share('setting', null);
         View::share('categories', []);
         View::share('brands', []);
-        View::share('stores', []);
-        View::share('countries', []);
-        View::share('divisions', []);
-        View::share('citys', []);
-        View::share('areas', []);
         View::share('admins', []);
 
         try {
+
             if (Schema::hasTable('settings')) {
                 View::share('setting', Setting::first());
             }
 
             if (Schema::hasTable('categories')) {
-                View::share('categories', Category::with('children','children.offers', 'offers', 'coupons')->whereNull('parent_id')->get());
+                View::share('categories', Category::with('children', 'children.offers', 'offers', 'coupons')->whereNull('parent_id')->get());
             }
 
             if (Schema::hasTable('brands')) {
                 View::share('brands', Brand::orderBy('name', 'asc')->get());
             }
 
-            if (Schema::hasTable('countries')) {
-                View::share('countries', Country::orderBy('name', 'asc')->get());
-            }
-
-            if (Schema::hasTable('divisions')) {
-                View::share('divisions', Division::orderBy('name', 'asc')->get());
-            }
-
-            if (Schema::hasTable('stores')) {
-                View::share('stores', Store::orderBy('title', 'asc')->get());
-            }
-
-            if (Schema::hasTable('cities')) {
-                View::share('citys', City::orderBy('name', 'asc')->get());
-            }
-
-            if (Schema::hasTable('areas')) {
-                View::share('areas', Area::orderBy('name', 'asc')->get());
-            }
-
             if (Schema::hasTable('admins')) {
                 View::share('admins', Admin::orderBy('name', 'asc')->get());
             }
+
         } catch (Exception $e) {
             // Log the exception if needed
         }
--- routes/api.php
+++ routes/api.php
@@ -8,11 +8,7 @@ use App\Http\Controllers\User\Api\UserApiController;
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
+
 */
 
 Route::post('/register', [UserApiController::class, 'register']);
