1db9f61743783abb04405f9d6fb29fcdf5efe51d
NgenItdev2 <dev2.ngenit@gmail.com>
2025-04-10 10:12:26 +0600

    update by ashiquzzaman 10-04-2025

--- resources/views/admin/pages/categories/index.blade.php
+++ resources/views/admin/pages/categories/index.blade.php
@@ -186,9 +186,14 @@
                                             </td>
                                             <td>
                                                 <!--begin::Status-->
-                                                <div
+                                                {{-- <div
                                                     class="badge {{ $child->status == 1 ? 'badge-light-success' : 'badge-light-danger' }}">
                                                     {{ $child->status == 1 ? 'Active' : 'InActive' }}
+                                                </div> --}}
+
+                                                <div
+                                                    class="badge {{ $child->status == 'active' ? 'badge-light-success' : 'badge-light-danger' }}">
+                                                    {{ $child->status == 'active' ? 'Active' : 'InActive' }}
                                                 </div>
                                                 <!--end::Status-->
                                             </td>
@@ -198,56 +203,6 @@
                                                     {{ $child->parent->name ?? 'N/A' }}
                                                     <!--end::Sl-->
                                             </td>
-
-                                            <!--begin::Action=-->
-                                            {{-- <td class="text-end">
-                                                <a href="#" class="btn btn-sm btn-light btn-active-light-primary"
-                                                    data-kt-menu-trigger="click"
-                                                    data-kt-menu-placement="bottom-end">Actions
-                                                    <!--begin::Svg Icon | path: icons/duotune/arrows/arr072.svg-->
-                                                    <span class="svg-icon svg-icon-5 m-0">
-                                                        <svg xmlns="http://www.w3.org/2000/svg" width="24"
-                                                            height="24" viewBox="0 0 24 24" fill="none">
-                                                            <path
-                                                                d="M11.4343 12.7344L7.25 8.55005C6.83579 8.13583 6.16421 8.13584 5.75 8.55005C5.33579 8.96426 5.33579 9.63583 5.75 10.05L11.2929 15.5929C11.6834 15.9835 12.3166 15.9835 12.7071 15.5929L18.25 10.05C18.6642 9.63584 18.6642 8.96426 18.25 8.55005C17.8358 8.13584 17.1642 8.13584 16.75 8.55005L12.5657 12.7344C12.2533 13.0468 11.7467 13.0468 11.4343 12.7344Z"
-                                                                fill="currentColor" />
-                                                        </svg>
-                                                    </span>
-                                                    <!--end::Svg Icon--></a>
-                                                <!--begin::Menu-->
-                                                <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-600 menu-state-bg-light-primary fw-bold fs-7 w-125px py-4"
-                                                    data-kt-menu="true">
-
-                                                    @if (Auth::guard('admin')->user()->can('show.category'))
-                                                        <!--begin::Menu item-->
-                                                        <div class="menu-item px-3">
-                                                            <a href="{{ route('admin.categories.show', $child->id) }}"
-                                                                class="menu-link px-3">Show</a>
-                                                        </div>
-                                                    @endif
-                                                    <!--end::Menu item-->
-
-                                                    @if (Auth::guard('admin')->user()->can('show.category'))
-                                                        <!--begin::Menu item-->
-                                                        <div class="menu-item px-3">
-                                                            <a href="{{ route('admin.categories.edit', $child->id) }}"
-                                                                class="menu-link px-3">Edit</a>
-                                                        </div>
-                                                    @endif
-                                                    <!--end::Menu item-->
-
-                                                    @if (Auth::guard('admin')->user()->can('show.category'))
-                                                        <!--begin::Menu item-->
-                                                        <div class="menu-item px-3">
-                                                            <a href="{{ route('admin.categories.destroy', $child->id) }}"
-                                                                class="menu-link px-3 delete">Delete</a>
-                                                        </div>
-                                                    @endif
-                                                    <!--end::Menu item-->
-
-                                                </div>
-                                                <!--end::Menu-->
-                                            </td> --}}
 
                                             <td>
 
