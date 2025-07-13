29e899df65456968d4581ab514002d79e57d3d70
shahed141123 <khandkershahed23@gmail.com>
2025-07-13 12:57:58 +0600

    21-06-25

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,17 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        'api/*', // Exclude all API routes from CSRF verification
+        'checkout-store', // Specific route for checkout store
+        'contact', // Specific route for contact form submission
+        'register', // Exclude registration route
+        'login', // Exclude login route
+        'reset-password/*', // Exclude reset password routes
+        'forgot-password', // Exclude forgot password route
+        'email-verification', // Exclude email verification route
+        'change-password', // Exclude change password route
+        'profile', // Exclude profile routes
+        'profile/edit', // Exclude profile edit route
+        'categories', // Exclude categories API route
     ];
 }
