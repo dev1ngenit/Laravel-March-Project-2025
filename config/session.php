db9d86bedf04d04d3d5e6c7d804a176b896dc29f
shahed141123 <khandkershahed23@gmail.com>
2025-07-16 11:17:05 +0600

    21-06-25

--- config/session.php
+++ config/session.php
@@ -128,7 +128,7 @@ return [
 
     'cookie' => env(
         'SESSION_COOKIE',
-        Str::slug(env('APP_NAME', 'laravel'), '_') . '_session'
+        Str::slug(env('APP_NAME', 'laravel'), '_').'_session'
     ),
 
     /*
@@ -155,9 +155,7 @@ return [
     |
     */
 
-    'domain' => env('SESSION_DOMAIN', null),
-
-    'same_site' => 'none',
+    'domain' => env('SESSION_DOMAIN'),
 
     /*
     |--------------------------------------------------------------------------
@@ -198,6 +196,6 @@ return [
     |
     */
 
-    // 'same_site' => 'lax',
+    'same_site' => 'lax',
 
 ];
