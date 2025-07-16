71513431e8a299cbeedace90701aab2de8942445
shahed141123 <khandkershahed23@gmail.com>
2025-07-16 11:06:25 +0600

    21-06-25

--- app/Providers/DynamicSessionServiceProvider.php
+++ app/Providers/DynamicSessionServiceProvider.php
@@ -18,8 +18,10 @@ class DynamicSessionServiceProvider extends ServiceProvider
         if (str_contains($host, 'micropack.vercel.app')) {
             Config::set('session.domain', '.micropack.vercel.app');
         } elseif (str_contains($host, 'accessories.ngengroup.org')) {
-            Config::set('session.domain', 'accessories.ngengroup.org');
+            Config::set('session.domain', '.ngengroup.org');
         } elseif (str_contains($host, 'localhost')) {
+            Config::set('session.domain', null); // ✅ Let browser handle it
+        } elseif (str_contains($host, '12')) {
             Config::set('session.domain', null); // ✅ Let browser handle it
         } else {
             Config::set('session.domain', null);
